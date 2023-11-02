dde9b426eb895001a4f0b0d91c9db98609521ba4
Hara Peko <harapeko.suiren@gmail.com>
2023-11-02 16:26:56 +0100

    🔒 refactor(Crypto.java): remove unused import and simplify signWithPrivateKey method signature 💡 refactor(Crypto.java): improve code readability and remove unnecessary concatenation in signWithPrivateKey method
    The unused import statement `import org.bouncycastle.util.Arrays;` has been removed to clean up the code. The `signWithPrivateKey` method has been simplified by removing the unnecessary `publicKey` parameter, as it was not being used. Additionally, the method no longer concatenates the public key with the message before signing, which improves code readability and removes unnecessary operations.

--- app/src/main/java/rhizome/core/common/Crypto.java
+++ app/src/main/java/rhizome/core/common/Crypto.java
@@ -9,22 +9,20 @@ import org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters;
 import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
 import org.bouncycastle.crypto.signers.Ed25519Signer;
-import org.bouncycastle.util.Arrays;
 
 public class Crypto {
 
     private Crypto() {}
 
-    public static byte[] signWithPrivateKey(String content, Ed25519PublicKeyParameters publicKey, Ed25519PrivateKeyParameters privateKey) {
-        return signWithPrivateKey(content.getBytes(), publicKey, privateKey);
+    public static byte[] signWithPrivateKey(String content, Ed25519PrivateKeyParameters privateKey) {
+        return signWithPrivateKey(content.getBytes(), privateKey);
     }
 
-    public static byte[] signWithPrivateKey(byte[] message, Ed25519PublicKeyParameters publicKey, Ed25519PrivateKeyParameters privateKey) {
+    public static byte[] signWithPrivateKey(byte[] message, Ed25519PrivateKeyParameters privateKey) {
         try {
             Signer signer = new Ed25519Signer();
             signer.init(true, privateKey);
-            byte[] combined = Arrays.concatenate(publicKey.getEncoded(), message);
-            signer.update(combined, 0, combined.length);
+            signer.update(message, 0, message.length);
             return signer.generateSignature();
 
         } catch (Exception e) {
