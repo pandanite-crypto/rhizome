5202ce0705e1efb499337a6c7dcaa5745c364723
Hara Peko <harapeko.suiren@gmail.com>
2023-11-03 00:27:24 +0100

    🔧 refactor(TransactionTests.java): refactor commented out test methods and improve code readability 🔬 test(TransactionTests.java): add test cases for transaction struct serialization and transaction copy The commit refactors the commented out test methods in the `TransactionTests` class to improve code readability. It also adds two new test cases: `checkTransactionStructSerialization` and `checkTransactionCopy`. The `checkTransactionStructSerialization` test case verifies the serialization and deserialization of a transaction struct, while the `checkTransactionCopy` test case checks the creation of a copy of a transaction. These test cases enhance the test coverage and ensure the correctness of the transaction-related functionality.

--- app/src/test/java/rhizome/TransactionTests.java
+++ app/src/test/java/rhizome/TransactionTests.java
@@ -38,71 +38,47 @@ class TransactionTests {
         assertEquals(ts, ((TransactionImpl) deserialized).getTimestamp());
     }
 
-    // @Test
-    // void checkTransactionStructSerialization() {
-    //     User miner = new User();
-    //     User receiver = new User();
+    @Test
+    void checkTransactionStructSerialization() {
+        User miner = User.create();
+        User receiver = User.create();
 
-    //     Transaction t = miner.mine();
-    //     Transaction t2 = miner.send(receiver, 30.0);
+        Transaction t = miner.mine();
+        Transaction t2 = miner.send(receiver, 30.0);
 
-    //     assertTrue(t2.signatureValid());
+        assertTrue(t2.signatureValid());
 
-    //     // Test the send transaction
-    //     long ts = t2.getTimestamp();
-    //     TransactionInfo serialized = t2.serialize();
-    //     Transaction deserialized = new Transaction(serialized);
+        // Test the send transaction
+        long ts = ((TransactionImpl) t2).getTimestamp();
+        TransactionInfo serialized = t2.serialize();
+        Transaction deserialized = Transaction.of(serialized);
 
-    //     assertTrue(deserialized.signatureValid());
-    //     assertEquals(t2, deserialized);
-    //     assertEquals(ts, deserialized.getTimestamp());
+        assertTrue(deserialized.signatureValid());
+        assertEquals(t2, deserialized);
+        assertEquals(ts, ((TransactionImpl) deserialized).getTimestamp());
 
-    //     // Test mining transaction
-    //     serialized = t.serialize();
-    //     deserialized = new Transaction(serialized);
-    //     ts = t.getTimestamp();
+        // Test mining transaction
+        serialized = t.serialize();
+        deserialized = Transaction.of(serialized);
+        ts = ((TransactionImpl) t).getTimestamp();
 
-    //     assertEquals(t.hashContents(), deserialized.hashContents());
-    //     assertEquals(t, deserialized);
-    //     assertEquals(ts, deserialized.getTimestamp());
-    // }
+        assertEquals(t.hashContents(), deserialized.hashContents());
+        assertEquals(t, deserialized);
+        assertEquals(ts, ((TransactionImpl) deserialized).getTimestamp());
+    }
 
-    // @Test
-    // void checkTransactionCopy() {
-    //     User miner = new User();
-    //     User receiver = new User();
+    @Test
+    void checkTransactionCopy() {
+        User miner = User.create();
+        User receiver = User.create();
 
-    //     Transaction t = miner.mine();
-    //     Transaction t2 = miner.send(receiver, 30.0);
+        Transaction t = miner.mine();
+        Transaction t2 = miner.send(receiver, 30.0);
 
-    //     Transaction a = new Transaction(t);
-    //     Transaction b = new Transaction(t2);
+        Transaction a = Transaction.of(t);
+        Transaction b = Transaction.of(t2);
 
-    //     assertEquals(a, t);
-    //     assertEquals(b, t2);
-    // }
-
-    // @Test
-    // void checkTransactionNetworkSerialization() {
-    //     User miner = new User();
-    //     User receiver = new User();
-
-    //     Transaction a = miner.mine();
-    //     Transaction b = miner.send(receiver, 30.0);
-
-    //     TransactionInfo t1 = a.serialize();
-    //     TransactionInfo t2 = b.serialize();
-
-    //     byte[] buf1 = new byte[TransactionInfo.BUFFER_SIZE];
-    //     byte[] buf2 = new byte[TransactionInfo.BUFFER_SIZE];
-
-    //     TransactionInfo.toBuffer(t1, buf1);
-    //     TransactionInfo.toBuffer(t2, buf2);
-
-    //     Transaction a2 = new Transaction(TransactionInfo.fromBuffer(buf1));
-    //     Transaction b2 = new Transaction(TransactionInfo.fromBuffer(buf2));
-
-    //     assertEquals(a, a2);
-    //     assertEquals(b, b2);
-    // }
+        assertEquals(a, t);
+        assertEquals(b, t2);
+    }
 }
