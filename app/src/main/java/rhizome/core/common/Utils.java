e368f92e3a430b47235fd80c5b14a731c89b40df
Hara Peko <harapeko.suiren@gmail.com>
2023-11-02 16:12:57 +0100

    🔧 chore(Utils.java): add utility methods for converting bytes to hex string and hex string to byte array The `bytesToHex` method converts a byte array to a hexadecimal string representation. This is useful for displaying binary data in a human-readable format.
    The `hexStringToByteArray` method converts a hexadecimal string to a byte array. This is useful for converting a hexadecimal representation of data back to its original binary form.
    
    These utility methods are added to the `Utils` class to provide convenient functionality for working with binary data in the application.

--- app/src/main/java/rhizome/core/common/Utils.java
+++ app/src/main/java/rhizome/core/common/Utils.java
@@ -224,4 +224,22 @@ public class Utils {
         }
         return buffer;
     }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                                 + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
 }
