d4fab4161c2f34e589b92c4cf9308ae5d64fb623
Hara Peko <harapeko.suiren@gmail.com>
2023-11-03 00:27:37 +0100

    🔧 chore(UserTests.java): import JSONObject class to fix compilation error ✨ feat(UserTests.java): add test case for user serialization and deserialization The JSONObject class is imported to fix a compilation error caused by its usage in the test case. A new test case `checkUserSerialization` is added to test the serialization and deserialization of a User object. The test case creates a Block, User objects, and performs transactions. It then serializes the User object to JSON, parses it back to a User object, and verifies that the signature still works. This test case ensures that the serialization and deserialization process does not affect the integrity of the User object.

--- app/src/test/java/rhizome/UserTests.java
+++ app/src/test/java/rhizome/UserTests.java
@@ -3,6 +3,7 @@ package rhizome;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static rhizome.core.common.Helpers.PDN;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import rhizome.core.block.Block;
@@ -22,6 +23,30 @@ class UserTests {
         Transaction t2 = miner.send(receiver, PDN(30.0));
         b.addTransaction(t2);
 
+        assertTrue(t2.signatureValid());
+    }
+
+    @Test
+    void checkUserSerialization() {
+        Block b = Block.empty();
+        
+        User miner = User.create();
+        User receiver = User.create();
+
+        Transaction t = miner.mine();
+        b.addTransaction(t);
+        Transaction t2 = miner.send(receiver, PDN(30.0));
+        b.addTransaction(t2);
+
+        assertTrue(t2.signatureValid());
+
+        // Recreate miner from JSON
+        String serialized = miner.toJson().toString();
+        JSONObject parsed = new JSONObject(serialized);
+
+        User minerCopy = User.of(parsed);
+        // Test the signature still works
+        minerCopy.signTransaction(t2);
         assertTrue(t2.signatureValid());
     }
 }
