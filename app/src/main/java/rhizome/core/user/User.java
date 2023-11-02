9d86a881d152117b0377e4f98c669459b203ef2e
Hara Peko <harapeko.suiren@gmail.com>
2023-11-03 00:27:06 +0100

    🔧 fix(User.java): remove unused imports and commented out code 🔧 fix(User.java): change User interface method `of` to static and update implementation to use a serializer 🔧 fix(User.java): remove unused `toJson` method and update implementation to use a serializer 🔧 fix(User.java): add missing `toJson` method implementation in UserImpl class 🔧 fix(UserImpl.java): add equals and hashCode methods to UserImpl class for proper object comparison The unused imports and commented out code have been removed from the User interface. The `of` method in the User interface has been changed to a static method and its implementation has been updated to use a serializer. The unused `toJson` method in the User interface has been removed and its implementation has been updated to use a serializer. The UserImpl class now implements the `toJson` method. The equals and hashCode methods have been added to the UserImpl class for proper object comparison.

--- app/src/main/java/rhizome/core/user/User.java
+++ app/src/main/java/rhizome/core/user/User.java
@@ -2,11 +2,19 @@ package rhizome.core.user;
 
 import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import rhizome.core.common.Utils.PublicWalletAddress;
+import rhizome.core.net.Serializable;
 import rhizome.core.transaction.Transaction;
 import rhizome.core.transaction.TransactionAmount;
 
 import static rhizome.core.common.Helpers.PDN;
+import static rhizome.core.common.Utils.privateKeyToString;
+import static rhizome.core.common.Utils.publicKeyToString;
+import static rhizome.core.common.Utils.stringToPrivateKey;
+import static rhizome.core.common.Utils.stringToPublicKey;
 import static rhizome.core.common.Crypto.generateKeyPair;
 
 public interface User {
@@ -19,19 +27,9 @@ public interface User {
                 .build();
     }
 
-    // Constructor from JSON object
-    // public User(JSONObject u) {
-    //     this.publicKey = stringToPublicKey(u.getString("publicKey"));
-    //     this.privateKey = stringToPrivateKey(u.getString("privateKey"));
-    // }
-
-    // Convert User object to JSON object
-    // public JSONObject toJson() {
-    //     JSONObject result = new JSONObject();
-    //     result.put("publicKey", publicKeyToString(this.publicKey));
-    //     result.put("privateKey", privateKeyToString(this.privateKey));
-    //     return result;
-    // }
+    public static User of(JSONObject json){
+        return serializer().fromJson(json);
+    }
 
     public Ed25519PublicKeyParameters getPublicKey();
     public Ed25519PrivateKeyParameters getPrivateKey();
@@ -49,14 +47,60 @@ public interface User {
     }
 
     default Transaction send(User to, TransactionAmount amount) {
-        PublicWalletAddress fromWallet = getAddress();
-        PublicWalletAddress toWallet = to.getAddress();
-        Transaction transaction = Transaction.of(fromWallet, toWallet, amount, getPublicKey());
-        signTransaction(transaction);
-        return transaction;
+        return Transaction.of(getAddress(), to.getAddress(), amount, getPublicKey())
+            .sign(getPublicKey(), getPrivateKey());
     }
 
     default void signTransaction(Transaction transaction) {
         transaction.sign(getPublicKey(), getPrivateKey());
+    }
+
+    public JSONObject toJson();
+    default JSONObject toJson(User transaction) {
+        return serializer().toJson(transaction);
+    }
+
+    static UserSerializer serializer(){
+        return UserSerializer.instance;
+    }
+
+    /**
+     * Serializes the Transaction
+     */
+    static class UserSerializer implements Serializable<JSONObject, User> {
+
+        static final String PUBLIC_KEY = "publicKey";
+        static final String PRIVATE_KEY = "privateKey";
+
+        static UserSerializer instance = new UserSerializer();
+
+        @Override
+        public JSONObject serialize(User object) {
+            throw new UnsupportedOperationException("Unimplemented method 'serialize'");
+        }
+        @Override
+        public User deserialize(JSONObject object) {
+            throw new UnsupportedOperationException("Unimplemented method 'deserialize'");
+        }
+        @Override
+        public User fromJson(JSONObject json) {
+            try {
+                return UserImpl.builder()
+                        .publicKey(stringToPublicKey(json.getString(PUBLIC_KEY)))
+                        .privateKey(stringToPrivateKey(json.getString(PRIVATE_KEY)))
+                        .build();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        public JSONObject toJson(User user) {
+            var userImpl = (UserImpl) user;
+            JSONObject result = new JSONObject();
+            result.put(PUBLIC_KEY, publicKeyToString(userImpl.getPublicKey()));
+            result.put(PRIVATE_KEY, privateKeyToString(userImpl.getPrivateKey()));
+            return result;
+        }
     }
 }
--- app/src/main/java/rhizome/core/user/UserImpl.java
+++ app/src/main/java/rhizome/core/user/UserImpl.java
@@ -1,7 +1,11 @@
 package rhizome.core.user;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 import org.bouncycastle.crypto.params.Ed25519PrivateKeyParameters;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
+import org.json.JSONObject;
 
 import lombok.Builder;
 import lombok.Data;
@@ -11,4 +15,23 @@ import lombok.Data;
 public class UserImpl implements User {
     private Ed25519PublicKeyParameters publicKey;
     private Ed25519PrivateKeyParameters privateKey;
+
+    @Override
+    public JSONObject toJson() {
+        return toJson(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Arrays.equals(getPublicKey().getEncoded(), user.getPublicKey().getEncoded()) &&
+            Arrays.equals(getPrivateKey().getEncoded(), user.getPrivateKey().getEncoded());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(publicKey, privateKey);
+    }
 }
