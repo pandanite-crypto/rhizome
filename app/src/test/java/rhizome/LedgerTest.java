92553b5b7fb5a129ff06b124d13b17cb32c6be90
Hara Peko <harapeko.suiren@gmail.com>
2023-11-04 09:54:59 +0100

    🚀 feat(LedgerTest.java): add LedgerTest class to test the functionality of the Ledger class The LedgerTest class is added to test the functionality of the Ledger class. It includes a test case to verify that the Ledger correctly stores wallets, deposits funds into wallets, and retrieves the value of a wallet. This test ensures the correctness of the Ledger implementation and helps maintain the integrity of the ledger functionality.

---
+++ app/src/test/java/rhizome/LedgerTest.java
@@ -0,0 +1,44 @@
+package rhizome;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static rhizome.core.common.Crypto.generateKeyPair;
+import static rhizome.core.common.Helpers.PDN;
+
+import java.io.IOException;
+
+import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import rhizome.core.common.Utils.PublicWalletAddress;
+import rhizome.core.ledger.Ledger;
+
+class LedgerTest {
+
+    private static final String TEST_DB_PATH = "./test-data/tmpdb";
+    private Ledger ledger;
+    private PublicWalletAddress wallet;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        // Simulate the generateKeyPair and walletAddressFromPublicKey functions
+        var pair = generateKeyPair();
+        wallet = PublicWalletAddress.fromPublicKey((Ed25519PublicKeyParameters) pair.getPublic());
+
+        ledger = new Ledger(TEST_DB_PATH);
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        ledger.closeDB();
+        ledger.deleteDB();
+    }
+
+    @Test
+    void testLedgerStoresWallets() {
+        ledger.createWallet(wallet);
+        ledger.deposit(wallet, PDN(50.0));
+        assertEquals(PDN(50.0), ledger.getWalletValue(wallet));
+    }
+}
