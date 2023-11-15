07f542eaefaabe3cbebf3781ec35086760a6e4c1
Hara Peko <harapeko.suiren@gmail.com>
2023-11-15 10:18:37 +0100

    🐛 fix(DataStore.java): change exception type from LedgerException to DataStoreException in clear() and close() methods ✨ feat(DataStore.java): add set() and get() methods to handle different types of data in the LevelDB store 🔧 chore(DataStore.java): add hasKey() and composeKey() methods for key existence check and key composition The exception type in the clear() and close() methods has been changed from LedgerException to DataStoreException to improve code clarity and maintain consistency. The set() and get() methods have been added to handle different types of data in the LevelDB store, allowing for more flexibility in storing and retrieving data. The hasKey() method has been added to check the existence of a key in the LevelDB store. The composeKey() method has been added to compose composite keys for more complex data structures.

--- app/src/main/java/rhizome/persistence/leveldb/DataStore.java
+++ app/src/main/java/rhizome/persistence/leveldb/DataStore.java
@@ -2,22 +2,31 @@ package rhizome.persistence.leveldb;
 
 import org.apache.commons.io.FileUtils;
 import org.iq80.leveldb.DB;
+import org.iq80.leveldb.DBException;
 import org.iq80.leveldb.Options;
+import org.iq80.leveldb.ReadOptions;
+import org.iq80.leveldb.WriteOptions;
 
+import io.activej.bytebuf.ByteBuf;
+import io.activej.bytebuf.ByteBufPool;
+import io.activej.common.MemSize;
 import lombok.Getter;
 import lombok.Setter;
-import rhizome.core.ledger.LedgerException;
+import lombok.extern.slf4j.Slf4j;
 
 import org.iq80.leveldb.DBIterator;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
 
 import static org.iq80.leveldb.impl.Iq80DBFactory.factory;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Getter
 @Setter
+@Slf4j
 public class DataStore {
     private DB db;
     private String path;
@@ -44,7 +53,7 @@ public class DataStore {
         try {
             FileUtils.deleteDirectory(directory);
         } catch (IOException e) {
-            throw new LedgerException("Could not clear path " + path, e);
+            throw new DataStoreException("Could not clear path " + path, e);
         }
     }
 
@@ -53,7 +62,7 @@ public class DataStore {
             try {
                 this.db.close();
             } catch (IOException e) {
-                throw new LedgerException("Could not close DataStore db", e);
+                throw new DataStoreException("Could not close DataStore db", e);
             }
             this.db = null;
         }
@@ -62,15 +71,110 @@ public class DataStore {
     public void clear() {
         try (DBIterator iterator = db.iterator()) {
             for (iterator.seekToFirst(); iterator.hasNext(); iterator.next()) {
-                String key = new String(iterator.peekNext().getKey(), StandardCharsets.UTF_8);
-                db.delete(key.getBytes(StandardCharsets.UTF_8));
+                String key = new String(iterator.peekNext().getKey(), UTF_8);
+                db.delete(key.getBytes(UTF_8));
             }
         } catch (IOException e) {
-            throw new LedgerException("Could not clear data store", e);
+            throw new DataStoreException("Could not clear data store", e);
         }
     }
 
     public String getPath() {
         return this.path;
+    }
+
+    protected void set(String key, String value) {
+        set(key.getBytes(UTF_8), value.getBytes(UTF_8));
+    }
+    protected void set(String key, int value) {
+        set(key.getBytes(UTF_8), Integer.toString(value).getBytes(UTF_8));
+    }
+    protected void set(String key, long value) {
+        var keyByte = ByteBufPool.allocate(Long.BYTES);
+        keyByte.writeLong(value);
+        set(key.getBytes(UTF_8), keyByte.asArray());
+    }
+    protected void set(String key, BigInteger value) {
+        set(key.getBytes(UTF_8), value.toByteArray());
+    }
+    protected void set(int key, byte[] value) {
+        var keyByte = ByteBufPool.allocate(Integer.BYTES);
+        keyByte.writeInt(key);
+        set(keyByte.asArray(), value);
+    }
+    protected void set(byte[] key, byte[] value) {
+        db.put(key, value, new WriteOptions().sync(true));
+    }
+
+    protected Object get(String key, Class<?> type) {
+        return get(key.getBytes(UTF_8), type);
+    }
+    protected Object get(int key, Class<?> type) {
+        var keyByte = ByteBufPool.allocate(Integer.BYTES);
+        keyByte.writeInt(key);
+        return get(keyByte.asArray(), type);
+    }
+    protected Object get(byte[] key, Class<?> type) {
+        var value = db.get(key, new ReadOptions());
+        if (value == null) {
+            throw new DataStoreException("Empty key: " + key);
+        }
+
+        if(type == String.class) {
+            return new String(value, UTF_8);
+        } else if (type == Integer.class) {
+            return ByteBuffer.wrap(value).getInt();
+        } else if (type == Long.class && value.length == Long.BYTES) {
+            return ByteBuffer.wrap(value).getLong();
+        } else if (type == BigInteger.class) {
+            return new BigInteger(value);
+        } else if (type == byte[].class) {
+            return value;
+        } else if (type == ByteBuf.class) {
+            var buff = ByteBufPool.allocate(MemSize.of(value.length));
+            buff.write(value);
+
+            if (!buff.canRead()) {
+                throw new DataStoreException("Could not read value of record " + key + " from BlockStore db.");
+            }
+
+            return buff;
+        } else {
+            throw new DataStoreException("Unsupported type");
+        }
+    }
+
+    protected boolean hasKey(String key) {
+        try {
+            return db.get(key.getBytes(UTF_8), new ReadOptions()) != null;
+        } catch (DBException e) {
+            log.error("Error checking key", e);
+            return false;
+        }
+    }
+
+    protected boolean hasKey(int key) {
+        try {
+            var keyByte = ByteBufPool.allocate(Integer.BYTES);
+            keyByte.writeInt(key);
+            return db.get(keyByte.asArray()) != null;
+        } catch (DBException e) {
+            log.error("Error checking key", e);
+            return false;
+        }
+    }
+
+    protected static byte[] composeKey(int key1, int key2) {
+        var key = ByteBufPool.allocate(MemSize.bytes(2l * Integer.BYTES));
+        key.writeInt(key1);
+        key.writeInt(key2);
+        return key.asArray();
+    }
+
+    protected static byte[] composeKey(byte[] key1, byte[] key2) {
+        var compositeKey = ByteBufPool.allocate(MemSize.bytes((long)key1.length + key2.length));
+        compositeKey.put(key1);
+        compositeKey.put(key2);
+        return compositeKey.asArray();
     }
 }
