3c877b80193f9f7fd3fe204d4bc7d205166f5f61
Hara Peko <harapeko.suiren@gmail.com>
2023-11-15 23:18:31 +0100

    📦 feat(BlockDto.java): add BlockDto class to represent a block in the blockchain The BlockDto class is added to the project. It implements the BinarySerializable interface and represents a block in the blockchain. It contains fields for the block's id, timestamp, difficulty, number of transactions, last block hash, merkle root, and nonce. The class also defines a constant BUFFER_SIZE for the size of the block in bytes.

---
+++ app/src/main/java/rhizome/core/block/dto/BlockDto.java
@@ -0,0 +1,42 @@
+package rhizome.core.block.dto;
+
+import org.jetbrains.annotations.NotNull;
+
+import io.activej.serializer.annotations.Deserialize;
+import io.activej.serializer.annotations.Serialize;
+import rhizome.core.net.BinarySerializable;
+
+public class BlockDto implements BinarySerializable {
+    @Serialize public final int id;
+    @Serialize public final long timestamp;
+    @Serialize public final int difficulty;
+    @Serialize public final int numTranactions;
+    @Serialize public final byte[] lastBlockHash;
+    @Serialize public final byte[] merkleRoot;
+    @Serialize public final byte[] nonce;
+
+    public static final int BUFFER_SIZE = 116;
+
+    public BlockDto(
+        @Deserialize("id") int id, 
+        @Deserialize("timestamp") long timestamp, 
+        @Deserialize("difficulty") int difficulty, 
+        @Deserialize("numTranactions") int numTranactions, 
+        @Deserialize("lastBlockHash") byte[] lastBlockHash, 
+        @Deserialize("merkleRoot") byte[] merkleRoot, 
+        @Deserialize("nonce") byte[] nonce) {
+
+        this.id = id;
+        this.timestamp = timestamp;
+        this.difficulty = difficulty;
+        this.numTranactions = numTranactions;
+        this.lastBlockHash = lastBlockHash;
+        this.merkleRoot = merkleRoot;
+        this.nonce = nonce;
+    }
+
+    @Override
+    public @NotNull int getSize() {
+        return BUFFER_SIZE;
+    }
+}
