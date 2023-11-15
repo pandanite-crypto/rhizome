5fe7b4fb39d2e280a962371c4441b3c8214e66a5
Hara Peko <harapeko.suiren@gmail.com>
2023-11-15 10:20:30 +0100

    🔧 refactor(BlockPersistence.java): remove unused methods and improve method naming for clarity 📝 chore(BlockPersistence.java): update method signatures for consistency and readability The unused `setBlockCount`, `setTotalWork`, `hasBlockCount`, `getRawData`, `removeBlockWalletTransactions`, and `setBlock` methods have been removed to improve code cleanliness. The method names have been updated to follow a more consistent and readable naming convention.

--- app/src/main/java/rhizome/persistence/BlockPersistence.java
+++ app/src/main/java/rhizome/persistence/BlockPersistence.java
@@ -3,8 +3,6 @@ package rhizome.persistence;
 import java.math.BigInteger;
 import java.util.List;
 
-
-import io.activej.bytebuf.ByteBuf;
 import rhizome.core.block.Block;
 import rhizome.core.block.BlockHeader;
 import rhizome.core.common.Utils.PublicWalletAddress;
@@ -12,17 +10,21 @@ import rhizome.core.common.Utils.SHA256Hash;
 import rhizome.core.transaction.TransactionInfo;
 
 public interface BlockPersistence {
-    void setBlockCount(long count) throws PersistenceException;
-    long getBlockCount() throws PersistenceException;
-    void setTotalWork(BigInteger count) throws PersistenceException;
-    BigInteger getTotalWork() throws PersistenceException;
-    boolean hasBlockCount() throws PersistenceException;
-    boolean hasBlock(int blockId) throws PersistenceException;
-    BlockHeader getBlockHeader(int blockId) throws PersistenceException;
-    List<TransactionInfo> getBlockTransactions(BlockHeader block) throws PersistenceException;
-    ByteBuf getRawData(int blockId) throws PersistenceException;
-    Block getBlock(int blockId) throws PersistenceException;
-    List<SHA256Hash> getTransactionsForWallet(PublicWalletAddress wallet) throws PersistenceException;
-    void removeBlockWalletTransactions(Block block) throws PersistenceException;
-    void setBlock(Block block) throws PersistenceException;
+    long getBlockCount();
+
+    BigInteger getTotalWork();
+
+    boolean hasBlock(int blockId);
+
+    BlockHeader getBlockHeader(int blockId);
+
+    List<TransactionInfo> getBlockTransactions(BlockHeader block);
+
+    Block getBlock(int blockId);
+
+    List<SHA256Hash> getTransactionsForWallet(PublicWalletAddress wallet);
+
+    void removeBlockWalletTransactions(Block block);
+    
+    void addBlock(Block block);
 }
