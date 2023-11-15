6e713215377516d941ff00e97a80b79442bb2f7c
Hara Peko <harapeko.suiren@gmail.com>
2023-11-15 23:18:45 +0100

    📦 feat(TransactionDto.java): add TransactionDto class to represent a transaction data transfer object The TransactionDto class is added to the project. It implements the BinarySerializable interface and represents a data transfer object for a transaction. It contains fields for signature, signing key, timestamp, to address, from address, amount, fee, and a flag indicating if the fee is a transaction fee. The class also defines a constant BUFFER_SIZE for the size of the serialized object.

---
+++ app/src/main/java/rhizome/core/transaction/dto/TransactionDto.java
@@ -0,0 +1,47 @@
+package rhizome.core.transaction.dto;
+
+import org.jetbrains.annotations.NotNull;
+
+import io.activej.serializer.annotations.Deserialize;
+import io.activej.serializer.annotations.Serialize;
+import lombok.Getter;
+import rhizome.core.net.BinarySerializable;
+
+@Getter
+public class TransactionDto implements BinarySerializable {
+    @Serialize public final String signature;
+    @Serialize public final String signingKey;
+    @Serialize public final long timestamp;
+    @Serialize public final byte[] to;
+    @Serialize public final byte[] from;
+    @Serialize public final long amount;
+    @Serialize public final long fee;
+    @Serialize public final boolean isTransactionFee;
+
+    public static final int BUFFER_SIZE = 149;
+
+    public TransactionDto(
+        @Deserialize("signature") String signature, 
+        @Deserialize("signingKey") String signingKey, 
+        @Deserialize("timestamp") long timestamp, 
+        @Deserialize("to") byte[] to, 
+        @Deserialize("from") byte[] from, 
+        @Deserialize("amount") long amount, 
+        @Deserialize("fee") long fee, 
+        @Deserialize("isTransactionFee") boolean isTransactionFee) {
+
+        this.signature = signature;
+        this.signingKey = signingKey;
+        this.timestamp = timestamp;
+        this.to = to;
+        this.from = from;
+        this.amount = amount;
+        this.fee = fee;
+        this.isTransactionFee = isTransactionFee;
+    }
+
+    @Override
+    public @NotNull int getSize() {
+        return BUFFER_SIZE;
+    }
+}
