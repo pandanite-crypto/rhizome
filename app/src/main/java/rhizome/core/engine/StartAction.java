6de54c036b1ac66a12ed919a4001c26d1d1c4abb
Hara Peko <harapeko.suiren@gmail.com>
2023-11-21 18:01:17 +0100

    🔧 chore(StartAction.java): remove unnecessary comment and newline at the end of the file The comment "// Créez un événement de démarrage" is removed as it is unnecessary and does not provide any useful information. Additionally, a newline at the end of the file is removed for consistency with the codebase.

--- app/src/main/java/rhizome/core/engine/StartAction.java
+++ app/src/main/java/rhizome/core/engine/StartAction.java
@@ -5,7 +5,7 @@ import io.activej.promise.Promise;
 class StartCommand implements Action {
     @Override
     public Promise<Void> execute(Task task) {
-        TaskEvent startEvent = new TaskEvent(); // Créez un événement de démarrage
+        TaskEvent startEvent = new TaskEvent();
         return task.getCurrentState().handleEvent(task, startEvent);
     }
 }
