f68c1204724e7224ea5bf5897efb701d4fc758c0
Hara Peko <harapeko.suiren@gmail.com>
2023-11-21 17:49:05 +0100

    🔧 chore(SHA256Hash.java): add utility methods for creating SHA256Hash from byte array and hex string, and for converting hash to hex string The utility methods `of(byte[] bytes)` and `of(String hexString)` are added to create a SHA256Hash object from a byte array and a hex string respectively. Additionally, a `toHexString()` method is added to convert the hash to a hex string representation. These utility methods improve the usability and flexibility of the SHA256Hash class.

--- app/src/main/java/rhizome/core/crypto/SHA256Hash.java
+++ app/src/main/java/rhizome/core/crypto/SHA256Hash.java
@@ -3,6 +3,9 @@ package rhizome.core.crypto;
 import io.activej.bytebuf.ByteBuf;
 import rhizome.core.common.SimpleHashType;
 
+import static rhizome.core.common.Utils.bytesToHex;
+import static rhizome.core.common.Utils.hexStringToByteArray;
+
 public record SHA256Hash (ByteBuf hash) implements SimpleHashType {
 
     public static SHA256Hash empty() {
@@ -11,6 +14,18 @@ public record SHA256Hash (ByteBuf hash) implements SimpleHashType {
 
     public static SHA256Hash random() {
         return new SHA256Hash(SimpleHashType.random(SIZE));
+    }
+
+    public static SHA256Hash of(byte[] bytes) {
+        return new SHA256Hash(ByteBuf.wrapForReading(bytes));
+    }
+
+    public static SHA256Hash of(String hexString) {
+        return SHA256Hash.of(hexStringToByteArray(hexString));
+    }
+
+    public String toHexString() {
+        return bytesToHex(hash.getArray());
     }
 
     @Override
