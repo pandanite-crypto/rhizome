cd5d2fea71c86c43132e319d2d8c2e849f2020aa
Hara Peko <harapeko.suiren@gmail.com>
2023-11-21 23:50:45 +0100

    🔥 refactor(BlockImpl.java): remove unused import statement for java.util.Arrays The import statement for java.util.Arrays is no longer used in the BlockImpl.java file. Removing this unused import statement improves code readability and reduces clutter.

--- app/src/main/java/rhizome/core/block/BlockImpl.java
+++ app/src/main/java/rhizome/core/block/BlockImpl.java
@@ -7,7 +7,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
 
 import org.json.JSONObject;
