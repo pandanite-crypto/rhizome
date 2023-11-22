8641a06d0dccecfd23bc8a37b07a4d1a7b2ce02e
Hara Peko <harapeko.suiren@gmail.com>
2023-11-22 22:57:06 +0100

    🔒 refactor(PublicKey.java): change PublicKey class to use Optional<Ed25519PublicKeyParameters> instead of Ed25519PublicKeyParameters directly 🔒 refactor(PublicKey.java): modify PublicKey class to handle empty keys and provide static factory methods for creating instances The PublicKey class has been modified to use `Optional<Ed25519PublicKeyParameters>` instead of `Ed25519PublicKeyParameters` directly. This change allows for the representation of empty keys by using `Optional.empty()`. Additionally, static factory methods `empty()` and `of(AsymmetricKeyParameter keyParameter)` have been added to create instances of PublicKey. These changes improve the flexibility and readability of the PublicKey class.

--- app/src/main/java/rhizome/core/crypto/PublicKey.java
+++ app/src/main/java/rhizome/core/crypto/PublicKey.java
@@ -1,39 +1,61 @@
 package rhizome.core.crypto;
 
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.params.Ed25519PublicKeyParameters;
 import rhizome.core.common.SimpleHashType;
 
 import static rhizome.core.common.Utils.bytesToHex;
 import static rhizome.core.common.Utils.hexStringToByteArray;
 
-public record PublicKey(Ed25519PublicKeyParameters key) implements SimpleHashType {
+import java.util.Optional;
+
+public record PublicKey(Optional<Ed25519PublicKeyParameters> key) implements SimpleHashType {
+
+    public static final int SIZE = 32;
+
+    public static PublicKey empty() {
+        return new PublicKey(Optional.empty());
+    }
+    
+    public static PublicKey of(AsymmetricKeyParameter keyParameter) {
+        if (keyParameter == null) {
+            return empty();
+        }
+        if (keyParameter instanceof Ed25519PublicKeyParameters) {
+            return new PublicKey(Optional.of((Ed25519PublicKeyParameters) keyParameter));
+        }
+        return empty();
+    }
 
     public static PublicKey of(byte[] bytes) {
-        return new PublicKey(new Ed25519PublicKeyParameters(bytes, 0));
+        if (bytes == null || bytes.length != SIZE) {
+            return empty();
+        }
+        return new PublicKey(Optional.of(new Ed25519PublicKeyParameters(bytes, 0)));
     }
 
     public static PublicKey of(String hexString) {
-        if ("".equals(hexString)) {
-            return null;
+        if (hexString == null || hexString.isEmpty()) {
+            return empty();
         }
         if (hexString.length() != 64) {
             throw new IllegalArgumentException("Invalid public key string length. Expected 64 characters for a 32-byte key.");
         }
-        return new PublicKey(new Ed25519PublicKeyParameters(hexStringToByteArray(hexString), 0));    
+        return new PublicKey(Optional.of(new Ed25519PublicKeyParameters(hexStringToByteArray(hexString), 0)));
     }
 
     public String toHexString() {
-        if (key == null) {
-            return "";
-        }
-        return bytesToHex(key.getEncoded() == null ? new byte[0] : key.getEncoded());
+        return key.map(pk -> bytesToHex(pk.getEncoded())).orElse("");
     }
 
     public byte[] toBytes() {
-        return key.getEncoded();
+        return key.map(Ed25519PublicKeyParameters::getEncoded).orElseGet(() -> new byte[SIZE]);
     }
 
-    public static final int SIZE = 32;
+    public Ed25519PublicKeyParameters get() {
+        return key.orElse(null);
+    }
+
     @Override
     public int getSize() {
         return SIZE;
