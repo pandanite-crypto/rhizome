cfe33f714b834f8b90156d12648ca8b7ec9b2c35
Hara Peko <harapeko.suiren@gmail.com>
2023-11-22 22:58:10 +0100

    📦 chore(SerializerDefPublicKey.java): add SerializerDefPublicKey class 📦 chore(SerializerDefTransactionSignature.java): add SerializerDefTransactionSignature class The SerializerDefPublicKey and SerializerDefTransactionSignature classes have been added to the project. These classes implement the BinarySerializer interface from the ActiveJ library and provide serialization and deserialization logic for the PublicKey and TransactionSignature classes respectively. This allows instances of PublicKey and TransactionSignature to be encoded into binary format and decoded back into objects.

---
+++ app/src/main/java/rhizome/core/net/SerializerDefTransactionSignature.java
@@ -0,0 +1,28 @@
+package rhizome.core.net;
+
+import io.activej.serializer.BinaryInput;
+import io.activej.serializer.BinaryOutput;
+import io.activej.serializer.BinarySerializer;
+import io.activej.serializer.CompatibilityLevel;
+import io.activej.serializer.CorruptedDataException;
+import io.activej.serializer.SimpleSerializerDef;
+import rhizome.core.transaction.TransactionSignature;
+
+public class SerializerDefTransactionSignature extends SimpleSerializerDef<TransactionSignature> {
+    @Override
+    protected BinarySerializer<TransactionSignature> createSerializer(int version, CompatibilityLevel compatibilityLevel) {
+        return new BinarySerializer<TransactionSignature>() {
+            @Override
+            public void encode(BinaryOutput out, TransactionSignature signature) {
+                out.write(signature.toBytes());
+            }
+
+            @Override
+            public TransactionSignature decode(BinaryInput in) throws CorruptedDataException {
+                byte[] signatureBytes = new byte[TransactionSignature.SIZE];
+                in.read(signatureBytes);
+                return TransactionSignature.of(signatureBytes);
+            }
+        };
+    }
+}
