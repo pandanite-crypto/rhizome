a276e1a4243cedffe1adeb216d4b83461154c1f3
Hara Peko <harapeko.suiren@gmail.com>
2023-11-25 00:15:02 +0100

    ✨ feat(ChainSync.java): add ChainSync class to handle synchronization of blockchain data The ChainSync class is responsible for synchronizing the blockchain data. It implements the Runnable interface to allow it to be executed in a separate thread. The class takes a HeaderChain object as a parameter in its constructor. In the run() method, it continuously checks if the block store cache has been tried and if the block store is not null. If these conditions are met, it retrieves the block hashes from the block store and updates the total work and chain length of the HeaderChain object. It then sets the tried block store cache flag to true and loads the chain. If the conditions are not met, it simply loads the chain. The thread sleeps for 10 seconds between each iteration.

---
+++ app/src/main/java/rhizome/core/blockchain/ChainSync.java
@@ -0,0 +1,33 @@
+package rhizome.core.blockchain;
+
+public class ChainSync implements Runnable {
+    private final HeaderChain chain;
+
+    public ChainSync(HeaderChain chain) {
+        this.chain = chain;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (!chain.isTriedBlockStoreCache() && chain.getBlockStore() != null) {
+                long chainLength = chain.getBlockStore().getBlockCount();
+                for (long i = 1; i <= chainLength; i++) {
+                    chain.getBlockHashes().add(chain.getBlockStore().getBlock((int)i).getHash());
+                }
+                chain.setTotalWork(chain.getBlockStore().getTotalWork());
+                chain.setChainLength(chainLength);
+                chain.setTriedBlockStoreCache(true);
+                chain.load();
+            } else {
+                chain.load();
+            }
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+    }
+}
