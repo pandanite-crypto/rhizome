aea9f0161e24c66150044e1d12c336fa5ef04bc1
Hara Peko <harapeko.suiren@gmail.com>
2023-11-26 23:48:07 +0100

    🔀 refactor(Pair.java): make Pair class implement Comparable interface and override necessary methods The Pair class now implements the Comparable interface and overrides the compareTo, equals, hashCode, and toString methods. This change allows instances of the Pair class to be compared, checked for equality, hashed, and represented as strings in a more meaningful way.

--- app/src/main/java/rhizome/core/common/Pair.java
+++ app/src/main/java/rhizome/core/common/Pair.java
@@ -1,6 +1,6 @@
 package rhizome.core.common;
 
-public class Pair<L, R> {
+public class Pair<L extends Comparable<L>, R> implements Comparable<Pair<L, R>> {
     private final L left;
     private final R right;
 
@@ -17,7 +17,36 @@ public class Pair<L, R> {
         return right;
     }
 
-    public static <L, R> Pair<L, R> of(L left, R right) {
+    public static <L extends Comparable<L>, R> Pair<L, R> of(L left, R right) {
         return new Pair<>(left, right);
     }
+
+    @Override
+	public int compareTo(Pair<L, R> o) {
+		return left.compareTo(o.left);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Pair<?, ?> pair = (Pair<?, ?>) o;
+
+		return left.equals(pair.left) && right.equals(pair.right);
+	}
+
+	@Override
+	public int hashCode() {
+		return 31 * left.hashCode() + right.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return "Pair{left=" + left + ", right=" + right + '}';
+	}
 }
