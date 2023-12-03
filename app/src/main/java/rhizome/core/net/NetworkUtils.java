08dd3fd9c1aefa9b3c85b5618a0322811dfbb629
Hara Peko <harapeko.suiren@gmail.com>
2023-12-03 23:32:42 +0100

    📦 chore(NetworkUtils.java): add NetworkUtils class to handle network-related operations 🔒 feat(NetworkUtils.java): implement computeAddress method to compute the address based on IP, port, and firewall flag The NetworkUtils class is added to the project to handle network-related operations. The computeAddress method takes an IP address, port number, and a firewall flag as input and computes the address based on these parameters. If the firewall flag is true, the address is set to "http://undiscoverable". Otherwise, the method uses a list of lookup services to retrieve the current IP address and constructs the address using the IP and port. If the IP is already provided, it is used directly. The method also includes a helper method isValidIPv4 to validate the IP address format.

---
+++ app/src/main/java/rhizome/core/net/NetworkUtils.java
@@ -0,0 +1,69 @@
+package rhizome.core.net;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import io.activej.eventloop.Eventloop;
+import io.activej.http.AsyncHttpClient;
+import io.activej.http.HttpRequest;
+import lombok.extern.slf4j.Slf4j;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@Slf4j
+public class NetworkUtils {
+
+    private NetworkUtils() {}
+
+    public static String computeAddress(String ip, int port, boolean firewall) {
+        if (firewall) {
+            return "http://undiscoverable";
+        }
+
+        var eventloop = Eventloop.create();
+        var httpClient = AsyncHttpClient.create(eventloop);
+        String address = "";
+        
+        if (ip.isEmpty()) {
+            boolean found = false;
+            List<String> lookupServices = Arrays.asList(
+                "http://checkip.amazonaws.com", 
+                "http://icanhazip.com", 
+                "http://ifconfig.co", 
+                "http://wtfismyip.com/text", 
+                "http://ifconfig.io"
+            );
+
+            for (String lookupService : lookupServices) {
+                try {
+                    String rawUrl = eventloop.submit(() ->
+				    httpClient.request(HttpRequest.get(lookupService))
+						.then(response -> response.loadBody())
+						.map(body -> body.getString(UTF_8))).get();
+                    ip = rawUrl.trim();
+                    if (isValidIPv4(ip)) {
+                        address = "http://" + ip + ":" + port;
+                        found = true;
+                        break;
+                    }
+                } catch (ExecutionException | InterruptedException e) {
+                    log.error(null, e);
+                }
+            }
+
+            if (!found) {
+                log.error("Could not determine current IP address");
+            }
+        } else {
+            address = ip + ":" + port;
+        }
+        return address;
+    }
+
+    public static boolean isValidIPv4(String ip) {
+        // String ipPattern = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+        var ipPattern = "^((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)$";
+        return ip.matches(ipPattern);
+    }
+}
