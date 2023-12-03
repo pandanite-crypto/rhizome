57fd727c9c9f0efdcbd3cf1d110b8777f1f0a312
Hara Peko <harapeko.suiren@gmail.com>
2023-12-03 23:35:52 +0100

    ✨ feat(ProtocolVersion.java): add ProtocolVersion enum to represent different versions of the protocol The ProtocolVersion enum is added to the `rhizome.core.net.protocol` package. It represents different versions of the protocol and currently only includes `V01` version. The enum provides methods to retrieve the code of a version, convert a code to a ProtocolVersion, check if a version is supported, and get a list of supported versions. This enum will be used to handle protocol versioning in the application.

---
+++ app/src/main/java/rhizome/core/net/protocol/ProtocolVersion.java
@@ -0,0 +1,46 @@
+package rhizome.core.net.protocol;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+public enum ProtocolVersion {
+    V01((byte) 1);
+
+    public static final byte LOWER = V01.getCode();
+    public static final byte UPPER = V01.getCode();
+
+    private final byte code;
+
+        ProtocolVersion(byte code) {
+        this.code = code;
+    }
+
+    public static ProtocolVersion fromCode(int code) {
+        for (ProtocolVersion v : values()) {
+            if (v.code == code) {
+                return v;
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean isSupported(byte code) {
+        return code >= LOWER && code <= UPPER;
+    }
+
+    public static List<ProtocolVersion> supported() {
+        List<ProtocolVersion> supported = Lists.newArrayList();
+        for (ProtocolVersion v : values()) {
+            if (isSupported(v.code)) {
+                supported.add(v);
+            }
+        }
+        return supported;
+    }
+
+    public byte getCode() {
+        return code;
+    }
+}
