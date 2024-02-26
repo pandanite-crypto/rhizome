58e7851393e0f4df83b88c802596a9ba9c15d2d0
Hara Peko <harapeko.suiren@gmail.com>
2024-02-26 15:19:37 +0000

    WIP

---
+++ app/src/main/java/rhizome/net/p2p/rpc/GossipStrategy.java
@@ -0,0 +1,106 @@
+package rhizome.net.p2p.rpc;
+
+import io.activej.async.callback.Callback;
+import io.activej.rpc.client.RpcClientConnectionPool;
+import io.activej.rpc.client.sender.DiscoveryService;
+import io.activej.rpc.client.sender.RpcSender;
+import io.activej.rpc.client.sender.RpcStrategy;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static io.activej.common.Checks.checkArgument;
+
+public class GossipStrategy implements RpcStrategy {
+	private final Random random = new Random();
+	private final Map<RpcStrategy, Integer> strategyToWeight = new HashMap<>();
+
+	private GossipStrategy() {}
+
+	public static GossipStrategy create() {return new GossipStrategy();}
+
+	public GossipStrategy add(int weight, RpcStrategy strategy) {
+		checkArgument(weight >= 0, "weight cannot be negative");
+		checkArgument(!strategyToWeight.containsKey(strategy), "withStrategy is already added");
+
+		strategyToWeight.put(strategy, weight);
+
+		return this;
+	}
+
+	@Override
+	public DiscoveryService getDiscoveryService() {
+		return DiscoveryService.combined(strategyToWeight.keySet().stream()
+				.map(RpcStrategy::getDiscoveryService)
+				.collect(Collectors.toList()));
+	}
+
+	@Override
+	public RpcSender createSender(RpcClientConnectionPool pool) {
+		Map<RpcSender, Integer> senderToWeight = new HashMap<>();
+		int totalWeight = 0;
+		for (Map.Entry<RpcStrategy, Integer> entry : strategyToWeight.entrySet()) {
+			RpcSender sender = entry.getKey().createSender(pool);
+			if (sender != null) {
+				int weight = entry.getValue();
+				senderToWeight.put(sender, weight);
+				totalWeight += weight;
+			}
+		}
+
+		if (totalWeight == 0) {
+			return null;
+		}
+
+		long randomLong = random.nextLong();
+		long seed = randomLong != 0L ? randomLong : 2347230858016798896L;
+
+		return new GossipSender(senderToWeight, seed);
+	}
+
+	private static final class GossipSender implements RpcSender {
+		private final List<RpcSender> senders;
+		private final int[] cumulativeWeights;
+		private final int totalWeight;
+
+		private long lastRandomLong;
+
+		GossipSender(Map<RpcSender, Integer> senderToWeight, long seed) {
+			assert !senderToWeight.containsKey(null);
+
+			senders = new ArrayList<>(senderToWeight.size());
+			cumulativeWeights = new int[senderToWeight.size()];
+			int currentCumulativeWeight = 0;
+			int currentSender = 0;
+			for (Map.Entry<RpcSender, Integer> entry : senderToWeight.entrySet()) {
+				currentCumulativeWeight += entry.getValue();
+				senders.add(entry.getKey());
+				cumulativeWeights[currentSender++] = currentCumulativeWeight;
+			}
+			totalWeight = currentCumulativeWeight;
+
+			lastRandomLong = seed;
+		}
+
+		@Override
+		public <I, O> void sendRequest(I request, int timeout, @NotNull Callback<O> cb) {
+			lastRandomLong ^= (lastRandomLong << 21);
+			lastRandomLong ^= (lastRandomLong >>> 35);
+			lastRandomLong ^= (lastRandomLong << 4);
+			int currentRandomValue = (int) ((lastRandomLong & Long.MAX_VALUE) % totalWeight);
+			int lowerIndex = 0;
+			int upperIndex = cumulativeWeights.length;
+			while (lowerIndex != upperIndex) {
+				int middle = (lowerIndex + upperIndex) / 2;
+				if (currentRandomValue >= cumulativeWeights[middle]) {
+					lowerIndex = middle + 1;
+				} else {
+					upperIndex = middle;
+				}
+			}
+			senders.get(lowerIndex).sendRequest(request, timeout, cb);
+		}
+	}
+}
---
+++ app/src/main/java/rhizome/server/RpcClientModule.java
@@ -0,0 +1,63 @@
+package rhizome.server;
+import io.activej.config.Config;
+import io.activej.config.converter.ConfigConverters;
+import io.activej.eventloop.Eventloop;
+import io.activej.inject.annotation.Provides;
+import io.activej.inject.module.AbstractModule;
+import io.activej.rpc.client.RpcClient;
+import io.activej.rpc.client.sender.RpcStrategy;
+import io.activej.serializer.SerializerBuilder;
+
+import java.net.InetSocketAddress;
+import java.time.Duration;
+import java.util.List;
+
+import static io.activej.common.Checks.checkState;
+import static io.activej.rpc.client.sender.RpcStrategies.*;
+
+public class RpcClientModule extends AbstractModule {
+	private RpcClientModule() {
+	}
+
+	public static RpcClientModule create() {
+		return new RpcClientModule();
+	}
+
+	@Provides
+	RpcClient rpcClient(Eventloop eventloop, RpcStrategy strategy) {
+		return RpcClient.create(eventloop)
+				.withConnectTimeout(Duration.ofSeconds(1))
+				.withSerializerBuilder(SerializerBuilder.create())
+				.withMessageTypes(Integer.class)
+				.withStrategy(strategy);
+	}
+
+	@Provides
+	RpcStrategy rpcStrategy(Config config) {
+		List<InetSocketAddress> inetAddresses = config.get(ConfigConverters.ofList(
+				ConfigConverters.ofInetSocketAddress(), ","), "client.addresses");
+		checkState(inetAddresses.size() == 4);
+
+		return firstAvailable(
+				rendezvousHashing(Object::hashCode)
+						.withShard(1, server(inetAddresses.get(0)))
+						.withShard(2, server(inetAddresses.get(1))),
+				rendezvousHashing(Object::hashCode)
+						.withShard(1, server(inetAddresses.get(2)))
+						.withShard(2, server(inetAddresses.get(3)))
+		);
+	}
+
+	@Provides
+	Eventloop eventloop() {
+		return Eventloop.create();
+	}
+
+	@Provides
+	Config config() {
+		return Config.create()
+				.with("protocol.compression", "false")
+				.with("client.addresses", "localhost:9000, localhost:9001, localhost:9002, localhost:9003")
+				.overrideWith(Config.ofSystemProperties("config"));
+	}
+}
