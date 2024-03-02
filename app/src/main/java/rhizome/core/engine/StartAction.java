5c5f1a70818363992572ad3a1d60e7e7ba0ab8c1
Hara Peko <harapeko.suiren@gmail.com>
2024-03-02 00:19:24 +0000

    Refactor StartCommand to StartAction

--- app/src/main/java/rhizome/core/engine/StartAction.java
+++ app/src/main/java/rhizome/core/engine/StartAction.java
@@ -2,7 +2,7 @@ package rhizome.core.engine;
 
 import io.activej.promise.Promise;
 
-class StartCommand implements Action {
+class StartAction implements Action {
     @Override
     public Promise<Void> execute(Task task) {
         TaskEvent startEvent = new TaskEvent();
