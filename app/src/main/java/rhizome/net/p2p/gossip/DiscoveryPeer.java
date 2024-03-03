d812e910245cbc37996ffdee7370997e28153ba8
Hara Peko <harapeko.suiren@gmail.com>
2024-03-03 22:34:45 +0000

    Add getPeerChannel method to DiscoveryPeer class

--- app/src/main/java/rhizome/net/p2p/gossip/DiscoveryPeer.java
+++ app/src/main/java/rhizome/net/p2p/gossip/DiscoveryPeer.java
@@ -4,6 +4,7 @@ import java.net.InetSocketAddress;
 import java.util.UUID;
 
 import rhizome.net.p2p.peer.Peer;
+import rhizome.net.p2p.peer.PeerChannel;
 import rhizome.net.p2p.peer.PeerState;
 
 public record DiscoveryPeer(String cluster, UUID id, InetSocketAddress address, PeerState state, long lastPingTime, long clockDelta, long version) implements Peer {
@@ -11,5 +12,11 @@ public record DiscoveryPeer(String cluster, UUID id, InetSocketAddress address, 
     @Override
     public Peer refresh(long startRequestTime) {
         return new DiscoveryPeer(cluster, id, address, state, System.currentTimeMillis() / 1000, System.currentTimeMillis() / 1000 - startRequestTime, version);
+    }
+
+    @Override
+    public PeerChannel getPeerChannel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPeerChannel'");
     }
 }
