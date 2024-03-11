359c62808f4a148630f05fd39d8930e7297e3c02
Hara Peko <harapeko.suiren@gmail.com>
2024-03-11 16:52:57 +0000

    Refactor GossipDiscovery for improved discovery process

--- app/src/main/java/rhizome/net/p2p/gossip/GossipDiscovery.java
+++ app/src/main/java/rhizome/net/p2p/gossip/GossipDiscovery.java
@@ -1,10 +1,13 @@
 package rhizome.net.p2p.gossip;
 
 import io.activej.async.callback.Callback;
+import io.activej.promise.Promise;
+import io.activej.promise.Promises;
 import lombok.Builder;
 import rhizome.net.p2p.DiscoveryService;
 import rhizome.net.p2p.PeerSystem;
 import rhizome.net.p2p.peer.Peer;
+import java.net.InetSocketAddress;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -15,59 +18,58 @@ import java.util.*;
 public class GossipDiscovery implements DiscoveryService {
 	private final PeerSystem peerSystem;
 	private Exception error;
+	private Map<Object, List<InetSocketAddress>> discovered;
+	private Map<Object, Peer> totalDiscovered;
 
 	@Override
 	public void discover(@Nullable Map<Object, Peer> previous, Callback<Map<Object, Peer>> cb) {
-		// if (error != null) {
-		// 	cb.accept(null, error);
-		// }
 
-		// if (discovered.size() == discoveryServicesSize && !totalDiscovered.equals(previous)) {
-		// 	cb.accept(totalDiscovered, null);
-		// }
+		previous.values().stream()
+			.map(this::doDiscover)
+			.forEach(
+				p -> p.whenComplete((result, e) -> {
+					if (e == null) {
+						onDiscover(result);
+					} else {
+						onError(e, cb);
+					}
+				})
+			);
 
-		// callbacks.add(cb);
+		if (error != null) {
+			cb.accept(null, error);
+		}
+
+		if (discovered.size() == totalDiscovered.size() && !totalDiscovered.equals(previous)) {
+			cb.accept(totalDiscovered, null);
+		}
 	}
 
-	private void doDiscover(Peer discoveryService) {
-		// discoveryService.discover(discovered.get(discoveryService), (result, e) -> {
-		// 	if (error != null) return;
-
-		// 	if (e == null) {
-		// 		onDiscover(discoveryService, result);
-		// 	} else {
-		// 		onError(e);
-		// 	}
-		// });
+	private Promise<Map<Object, Peer>> doDiscover(Peer peer) {
+		return peerSystem.getPeers(peer);
 	}
 
-	private void onError(@NotNull Exception e) {
+	private void onError(@NotNull Exception e, Callback<Map<Object, Peer>> cb) {
 		error = e;
-		completeCallbacks();
+		if (error != null) {
+			cb.accept(null, error);
+		} else {
+			cb.accept(discovered, error);
+		}
 	}
 
-	private void onDiscover(DiscoveryService discoveryService, Map<Object, Peer> discovered) {
-		// Map<Object, Peer> old = this.discovered.put(discoveryService, discovered);
+	private void onDiscover(Map<Object, Peer> discovered) {
+		Map<Object, Peer> old = this.discovered.put(discovered);
 
-		// Map<Object, Peer> newTotalDiscovered = new HashMap<>(totalDiscovered);
-		// if (old != null) {
-		// 	newTotalDiscovered.keySet().removeAll(old.keySet());
-		// }
-		// newTotalDiscovered.putAll(discovered);
-		// this.totalDiscovered = Collections.unmodifiableMap(newTotalDiscovered);
+		Map<Object, Peer> newTotalDiscovered = new HashMap<>(totalDiscovered);
+		if (old != null) {
+			newTotalDiscovered.keySet().removeAll(old.keySet());
+		}
+		newTotalDiscovered.putAll(discovered);
+		this.totalDiscovered = Collections.unmodifiableMap(newTotalDiscovered);
 
-		// if (discovered.size() == discoveryServicesSize) {
-		// 	completeCallbacks();
-		// }
-	}
-
-	private void completeCallbacks() {
-		// Set<Callback<Map<Object, Peer>>> callbacks = new HashSet<>(this.callbacks);
-		// this.callbacks.clear();
-		// if (error != null) {
-		// 	callbacks.forEach(cb -> cb.accept(null, error));
-		// } else {
-		// 	callbacks.forEach(cb -> cb.accept(totalDiscovered, error));
-		// }
+		if (discovered.size() == discoveryServicesSize) {
+			completeCallbacks();
+		}
 	}
 }
