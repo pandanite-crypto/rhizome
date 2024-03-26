794c3ddcfbba909dfdb454a19fe2eaf5d31ad187
Hara Peko <harapeko.suiren@gmail.com>
2024-03-26 11:29:58 +0000

    Refactor DiscoveryService interface

--- app/src/main/java/rhizome/net/p2p/DiscoveryService.java
+++ app/src/main/java/rhizome/net/p2p/DiscoveryService.java
@@ -1,33 +1,30 @@
 package rhizome.net.p2p;
 
 import io.activej.async.callback.Callback;
-import rhizome.net.p2p.gossip.GossipDiscovery;
 import rhizome.net.p2p.peer.Peer;
 
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This interface defines the discovery service.
+ */
 public interface DiscoveryService {
 
+	/**
+	 * This method discovers the peers.
+	 * @param previous
+	 * @param cb
+	 */
     void discover(@Nullable Map<Object, Peer> previous, Callback<Map<Object, Peer>> cb);
 
-    // static DiscoveryService randomized(Map<Object, Peer> peers, PeerSystem peerSystem) {
-
-	// 	Map<Object, Peer> initialPeers = Collections.unmodifiableMap(new HashMap<>(peers));
-	// 	Map<Object, Peer> currentPeers = new HashMap<>(initialPeers);
-
-	// 	return (newPeers, cb) -> {
-
-			
-	// 		if (!initialPeers.equals(newPeers)) {
-	// 			cb.accept(newPeers, null);
-	// 		}
-	// 	};
-	// }
-
+	/**
+	 * This method creates a discovery service.
+	 * @param peers
+	 * @param peerSystem
+	 * @return
+	 */
 	static DiscoveryService create(Map<Object, Peer> peers, PeerSystem peerSystem) {
 		return peerSystem.getDiscoveryService(peers, peerSystem);
 	}
